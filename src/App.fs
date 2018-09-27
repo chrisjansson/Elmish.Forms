79913d400d97fe8a8e25a3ea7ff0f86f4e329fc7
chris <chris.janssom@gmail.com>
2018-09-27 07:36:32 +0200

    Code as shown in post 2

--- src/App.fs
+++ src/App.fs
@@ -6,30 +6,45 @@ open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.Core.JsInterop
 
+type Field = string
+type FieldId = string
+
 type Model =
     {
-        FirstName: string
-        LastName: string
+        Fields: Map<FieldId, Field>
         Result: string option
     }
 
 type Msg =
-    | InputChanged of string * string
+    | InputChanged of FieldId * string
     | Submit
 
 let init() : Model = {
-    FirstName = ""
-    LastName = ""
+    Fields = Map.empty
     Result = None    
 }
 
+module FieldId =
+    let create (id: string): FieldId = id
+
+module Field =
+    let defaultValue: Field = ""
+
+module Form =
+    let getField (id: FieldId) (model: Model): Field = 
+        Map.tryFind id model.Fields 
+        |> Option.defaultValue Field.defaultValue
+
+
+let firstNameId = FieldId.create "firstName"
+let lastNameId = FieldId.create "lastName"
+
 let update (msg:Msg) (model:Model) =
     match msg with
-    | InputChanged ("firstName", value) -> { model with FirstName = value }
-    | InputChanged ("lastName", value) -> { model with LastName = value }
+    | InputChanged (id, value) -> 
+        { model with Fields = Map.add id value model.Fields }
     | Submit -> 
-        { model with Result = Some <| sprintf "%A" (model.FirstName, model.LastName) }
-    | _ -> model
+        { model with Result = Some <| sprintf "%A" (Form.getField firstNameId model, Form.getField lastNameId model) }
 
 let view (model:Model) dispatch =
     let onChange field (event: Fable.Import.React.FormEvent) =
@@ -45,11 +60,11 @@ let view (model:Model) dispatch =
             form [ OnSubmit onSubmit ] [
                 div [] [
                     label [] [ unbox "First name" ]
-                    input [ Value model.FirstName; onChange "firstName" |> OnChange ]
+                    input [ Form.getField firstNameId model |> Value; onChange "firstName" |> OnChange ]
                 ]
                 div [] [
                     label [] [ unbox "Last name" ]
-                    input [ Value model.LastName; onChange "lastName" |> OnChange ]
+                    input [ Form.getField lastNameId model |> Value; onChange "lastName" |> OnChange ]
                 ]
                 button [ Type "submit" ] [ unbox "Submit" ]
             ]
