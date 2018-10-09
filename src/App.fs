5736e1b43ff87f0e6f37f9ce0b4ad8ffedc9304e
chris <chris.janssom@gmail.com>
2018-10-09 16:50:23 +0200

    Extract Result utils

--- src/App.fs
+++ src/App.fs
@@ -1,37 +1,11 @@
 module App
 
+open Extensions
 open Elmish
 open Elmish.React
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 open Fable.Core.JsInterop
-
-type ResultBuilder() =
-    member this.Bind(x, f) =
-        match x with
-        | Ok x -> f(x) 
-        | Error e -> Error e
-    member this.Delay(f) = f()
-    member this.Return(x) = Ok x
-
-let result = new ResultBuilder()
-
-module Result =
-    // M(a -> b) -> M a -> M b
-    let apply fr ar =
-        match (fr, ar) with
-        | (Ok f, Ok a) -> f a |> Ok
-        | (Error e, Ok _) -> Error e
-        | (Ok _, Error e) -> Error e
-        | (Error e1, Error e2) -> List.concat [ e1; e2 ] |> Error
-
-    let (<*>) f a = apply f a
-
-    let ret a = Ok a    
-
-    let lift2 f a b = ret f <*> a <*> b
-    let lift3 f a b c = ret f <*> a <*> b <*> c
-    let lift4 f a b c d = ret f <*> a <*> b <*> c <*> d
 
 type Field = string
 type FieldId = string
