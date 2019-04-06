0a0fc95e4a5c059b45dc85ef9d0a8279d7a2f83d
chris <chris.janssom@gmail.com>
2019-04-06 10:38:49 +0200

    More cleanup

--- src/Forms.fs
+++ src/Forms.fs
@@ -10,7 +10,6 @@ module Forms
         type KeyedValidationError = FieldId * ValidationError
         type ValidationResult<'a> = Result<'a, KeyedValidationError list>
         
-
         type Field =
             | Group of Group
             | List of Group list
@@ -60,7 +59,6 @@ module Forms
 
     module Form =
         open Model
-        open Extensions          
 
         let rec private find (path: Path.PathSegment list) (field: Model.Field) =
                 match (path, field) with
@@ -174,29 +172,7 @@ module Forms
             getValidationErrors id model |> List.isEmpty |> not
 
         let isTouched (id: FieldId) (model: Model<_>): bool =
-            Set.contains id model.Touched
-
-        let map2 f a b (model: Model<_>) =
-            let fa = Result.lift2 f
-            let a' = a model
-            let b' = b model
-            fa a' b'
-
-        let map3 f a b c (model: Model<_>) =
-            let fa = Result.lift3 f
-            let a' = a model
-            let b' = b model
-            let c' = c model
-            fa a' b' c'
-
-        let map4 f a b c d (model: Model<_>) =
-            let fa = Result.lift4 f
-            let a' = a model
-            let b' = b model
-            let c' = c model
-            let d' = d model
-            fa a' b' c' d'
-            
+            Set.contains id model.Touched        
     
     module Validator =
         type Validator<'T> = Validator of (Model.Group -> Model.ValidationResult<'T>)
@@ -253,7 +229,6 @@ module Forms
                 | _ -> Error [ (id, (fun _ -> "Invalid group type"))  ]
             Validator inner
 
-        
         let text id =
             let inner (f: Model.Group) =
                 match Map.tryFind id f with
@@ -284,106 +259,3 @@ module Forms
             v form.Fields
                        
     let (<*>) = Validator.apply
-//                       
-//    module FieldDefinition =
-//        open Model
-//        let private createDefaultOptionalFieldValidator (id: FieldId) =
-//            fun model -> 
-//                let field = Form.getField id model
-//                if field.Length = 0 then
-//                    None |> Ok
-//                else    
-//                    Some field |> Ok
-//
-//        let define (id: string): FieldDefinition<string option, _> =
-//            let fieldId: FieldId = FieldId.create id
-//            {
-//                Id = fieldId
-//                Name = id
-//                Validate = createDefaultOptionalFieldValidator fieldId
-//            }
-//
-//        let withName (name: FieldName) (field: FieldDefinition<_, _>)  =
-//            { field with Name = name }
-//
-//        let isRequired (field: FieldDefinition<'a option, _>) =
-//            let requiredValidation model =
-//                match field.Validate model with
-//                | Ok (Some v) -> Ok v
-//                | Ok None -> 
-//                    let requiredErrorMessage = sprintf "%s is required" field.Name
-//                    Error [ (field.Id, requiredErrorMessage) ]
-//                | Error e -> Error e
-//            { 
-//                Id = field.Id
-//                Name = field.Name
-//                Validate = requiredValidation 
-//            }        
-//
-//        let minimumLength (length: int) (field: FieldDefinition<string option, _>) =
-//            let lengthValidation model =
-//                match field.Validate model with
-//                | Ok (Some v) -> 
-//                    if v.Length >= length then
-//                        v |> Some |> Ok
-//                    else                 
-//                        let errorMessage = sprintf "%s must be at least %i characters" field.Name length
-//                        Error [ (field.Id, errorMessage) ]
-//                | Ok None -> Ok None                
-//                | Error e -> Error e            
-//            {
-//                Id = field.Id
-//                Name = field.Name
-//                Validate = lengthValidation
-//            }        
-//
-//        let int (field: FieldDefinition<string option, _>) =
-//            let intValidation model =
-//                match field.Validate model with
-//                | Ok (Some v) -> 
-//                    match System.Int32.TryParse(v) with
-//                    | (true, i) -> i |> Some |> Ok
-//                    | _ -> 
-//                        let errorMessage = sprintf "%s must be an integer" field.Name
-//                        Error [ (field.Id, errorMessage) ]
-//                | Ok None -> Ok None                
-//                | Error e -> Error e            
-//            {
-//                Id = field.Id
-//                Name = field.Name
-//                Validate = intValidation
-//            }        
-//
-//        let greaterThan (limit: int) (field: FieldDefinition<int option, _>) =
-//            let limitValidation model =
-//                match field.Validate model with
-//                | Ok (Some v) -> 
-//                    if v >= limit then
-//                        v |> Some |> Ok
-//                    else                 
-//                        let errorMessage = sprintf "%s must be at greater than %i" field.Name limit
-//                        Error [ (field.Id, errorMessage) ]
-//                | Ok None -> Ok None                
-//                | Error e -> Error e            
-//            {
-//                Id = field.Id
-//                Name = field.Name
-//                Validate = limitValidation
-//            }        
-//
-//        let predicate (pred: 'a -> bool) (buildMessage: string -> string) (field: FieldDefinition<'a option, _>) =
-//            let validate model =
-//                match field.Validate model with
-//                | Ok (Some v) ->
-//                    if pred v then
-//                        v |> Some |> Ok
-//                    else
-//                        let errorMessage = buildMessage field.Name
-//                        Error [ (field.Id, errorMessage) ]                
-//                | Ok None -> Ok None                
-//                | Error e -> Error e                
-//            {
-//                Id = field.Id
-//                Name = field.Name
-//                Validate = validate
-//            }        
