2f873b0b86fe1a3d49428d0326056e7e4b46e452
Chris Jansson <chris.janssom@gmail.com>
2020-05-13 07:54:24 +0200

    Scribbled some notes

--- Elmish.Forms/Library.fs
+++ Elmish.Forms/Library.fs
@@ -1,5 +1,22 @@
 ﻿namespace Elmish.Forms
 
+//Validation error formatter
+// 'ValidationErrorContext -> string list
+
+//A validation function takes a validation environment, the value and returns a new value
+// 'value_in -> 'env -> Result<'value_out, ValidationErrorFormatter>
+
+//A validator
+//Validator<'Result, 'Env, 'InitializeFrom>
+// {
+//    Validate: FormData -> 'Env -> Result
+//    Schema: A description of the forms structure, includes metadata such as labels, type, requiredness, static default values
+//
+//
+//
+//
+// }
+
 module Say =
     let hello name =
         printfn "Hello %s" name
