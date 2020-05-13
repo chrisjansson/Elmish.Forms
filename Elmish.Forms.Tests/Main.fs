f1ea99bc41d83e60106139bdba875248bdce22f9
Chris Jansson <chris.janssom@gmail.com>
2020-05-13 07:33:52 +0200

    Add project structure

---
+++ Elmish.Forms.Tests/Main.fs
@@ -0,0 +1,6 @@
+﻿module ElmishForms.Tests
+open Expecto
+
+[<EntryPoint>]
+let main argv =
+    Tests.runTestsInAssembly defaultConfig argv
---
+++ Elmish.Forms/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace Elmish.Forms
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
