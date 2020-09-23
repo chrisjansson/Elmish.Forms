1188636c802fdcd7e585bc62825a8b0e3606e550
Chris Jansson <chris.janssom@gmail.com>
2020-09-23 17:44:38 +0200

    Style outbut box

--- Elmish.Forms.Samples/src/App.fs
+++ Elmish.Forms.Samples/src/App.fs
@@ -6,17 +6,12 @@ open Feliz
 
 type SampleModel =
     {
-        Submitted: string option
+        Submitted: Result<string, string list> option
     }
 
 module SimpleSample =
     open Elmish.Forms
 
-    let model: SampleModel =
-        {
-            Submitted = None
-        }
-        
     let render = App.Samples.ApplicativeSample.render
     let validator = App.Samples.ApplicativeSample.validator
     let source = App.Samples.ApplicativeSample.source
@@ -31,11 +26,7 @@ module SimpleSample =
                         render ()
                         
                         Html.div [ Html.button [ prop.type' "submit"; prop.text "Submit" ] ]
-                        
-                        if Option.isSome model.Submitted then
-                            Html.pre [ prop.text model.Submitted.Value ]
                     ]
-
                 ]
             )
 
@@ -43,13 +34,49 @@ module SimpleSample =
         React.functionComponent(fun () ->
             let model, setModel = React.useState({ Submitted = None })
             
-            let onSubmit result =
-                setModel({ model with Submitted = Some (Fable.Core.JS.JSON.stringify(result)) })
+            let onSubmit (result: Result<_, ValidationErrors>) =
+                let resultString =
+                    match result with
+                    | Ok r -> Fable.Core.JS.JSON.stringify(r) |> Ok
+                    | Error e ->
+                        e
+                        |> List.collect (fun (id, list) -> list)
+                        |> Error
+                
+                
+                setModel(
+                            { model with Submitted = Some resultString }
+                        )
             
             React.fragment [
                 React.form { Validator = validator; Render = renderForm; OnSubmit = onSubmit }
                 if model.Submitted.IsSome then
-                    Html.pre [ Html.text model.Submitted.Value ]
+                    match model.Submitted.Value with
+                    | Ok r ->
+                        Html.pre [
+                            prop.style [
+                                style.borderColor "green"
+                                style.backgroundColor "rgb(221, 255, 216)"
+                            ]
+                            prop.children [
+                                Html.text r
+                            ]
+                        ]
+                    | Error r ->
+                        Html.pre [
+                            prop.style [
+                                style.borderColor "red"
+                                style.backgroundColor "#fff6f6"
+                            ]
+                            prop.children [
+                                Html.unorderedList [
+                                    for e in r do
+                                        Html.li [ Html.text e ]
+                                ]
+                            ]
+                  
+                        ]
+                        
                 Html.pre [ Html.text source ]
             ]
         )
